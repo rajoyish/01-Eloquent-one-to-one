f6a16ab126e258dd6fb961d42ed316ff67e5cee4
Himalayan <hdnpress@gmail.com>
2023-01-24 13:54:06 +0545

    One to one relationship between Phone & User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -49,4 +49,9 @@ class User extends Authenticatable
     {
         return $this->hasOne(Company::class);
     }
+
+    public function phone(): HasOne
+    {
+        return $this->hasOne(Phone::class);
+    }
 }
